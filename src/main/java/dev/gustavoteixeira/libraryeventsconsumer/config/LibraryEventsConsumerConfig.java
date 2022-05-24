5c48509b707f4d42ace5558bdefdc951eb2ded64
Gustavo Teixeira <contact@gustavoteixeira.dev>
2022-05-24 00:35:33 -0300

    added code

---
+++ src/main/java/dev/gustavoteixeira/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,10 @@
+package dev.gustavoteixeira.libraryeventsconsumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@Configuration
+public class LibraryEventsConsumerConfig {
+
+}
---
+++ src/main/java/dev/gustavoteixeira/libraryeventsconsumer/consumer/LibraryEventsConsumer.java
@@ -0,0 +1,18 @@
+package dev.gustavoteixeira.libraryeventsconsumer.consumer;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class LibraryEventsConsumer {
+
+    @KafkaListener(topics = {"library-events"})
+    public void onMessage(ConsumerRecord<Integer, LibraryEvent> consumerRecord) {
+        log.info("ConsumerRecord: {}", consumerRecord.value());
+    }
+
+}
