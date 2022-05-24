48509d95057723a7f75045165486010d12646e62
Gustavo Teixeira <contact@gustavoteixeira.dev>
2022-05-23 22:11:02 -0300

    first commit

---
+++ src/test/java/dev/gustavoteixeira/libraryeventsconsumer/LibraryEventsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gustavoteixeira.libraryeventsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
